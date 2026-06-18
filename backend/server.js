5f33a762f60b56bcafb2c3bf07185ebd075f3adc
divyalahoti <divyalavati01@gmail.com>
2026-06-18 11:04:05 +0530

    Cors  Updated

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,37 @@ dotenv.config();
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
+
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  "http://localhost:3000",
+  "https://chocobitsbackend.vercel.app/",
+  "https://chocobitsfrontend.vercel.app/"
+
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow non-browser tools like Postman
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.error(' CORS blocked origin:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // if using cookies or Authorization headers
+};
+
+app.use(cors(corsOptions));
+
+
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
